299677e4c790ffb083b0da30d146b6ceccfe9112
gonzapereyra <gonza.pereyra0912@gmail.com>
2025-04-02 14:42:36 -0300

    creado archivo homepage

--- src/main/java/org/example/HomePage.java
+++ src/main/java/org/example/HomePage.java
@@ -1,8 +1,13 @@
 package org.example;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePage {
     private WebDriver driver;
 
-}
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    }
