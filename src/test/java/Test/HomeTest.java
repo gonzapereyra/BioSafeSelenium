8498579dbba054450544b4dec4d5a1f332a49fa8
gonzapereyra <gonza.pereyra0912@gmail.com>
2025-04-14 15:14:41 -0300

    agregado de localizadores y manipulacion del registro

--- src/test/java/Test/HomeTest.java
+++ src/test/java/Test/HomeTest.java
@@ -1,11 +1,20 @@
 package Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.DateTimeException;
+import java.time.Duration;
 
 public class HomeTest {
     private WebDriver driver;
@@ -21,6 +30,40 @@ public class HomeTest {
         driver.get("https://qa.biosafeapp.com/");
         String title = driver.getTitle();
         Assert.assertEquals(title,"BioSafeApp");
+
+        //Utilizacion de localizadores para completar el registro de usuario
+
+
+        //Localizarel boton de registro y realizar click:
+        driver.findElement(By.xpath("(//a[@href='/signup'])[2]")).click();
+
+        //Localizar los inputs necesarios para completar el formulario:
+        driver.findElement(By.xpath("//input[@data-testid='nameInput']")).sendKeys("Gonzalo");//Nombre
+        driver.findElement(By.xpath("//input[@data-testid='emailInput']")).sendKeys("Gonzalo"); // Mail
+        driver.findElement(By.xpath("//input[@data-testid='passwordInput']")).sendKeys("Gonzalo"); //Contraseña
+        driver.findElement(By.xpath("(//img[@class='toggle-password'])[1]")).click(); //visor de contraseña
+
+        //Generar un tiempo de espera o pausa, este tiempo es solo para demorar la carga de datos y que se puedan ver las acciones
+        try {
+            Thread.sleep(1000); // 2 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace(); // Podés loguearlo o manejarlo de otra forma si querés
+        }
+
+        driver.findElement(By.xpath("(//img[@class='toggle-password'])[1]")).click(); // visor contraseña
+        driver.findElement(By.xpath("//input[@data-testid='confirmPasswordInput']")).sendKeys("Gonzalo"); // confirmar contraseña
+        driver.findElement(By.xpath("(//img[@class='toggle-password'])[2]")).click(); // visor confirmar contraseña
+
+        //Generar un tiempo de espera o pausa, este tiempo es solo para demorar la carga de datos y que se puedan ver las acciones
+        try {
+            Thread.sleep(1000); // 2 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace(); // Podés loguearlo o manejarlo de otra forma si querés
+        }
+
+        driver.findElement(By.xpath("(//img[@class='toggle-password'])[2]")).click();//visor confirmar contraseña
+
+
     }
 
 
